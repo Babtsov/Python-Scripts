8849b40e44d345e11a0c43caf1b74ed728ec5ba9
Ben Babtsov <ben.babtsov@ufl.edu>
2015-08-23 01:43:38 -0400

    improved .csv file matching using regex

--- emailExtract.py
+++ emailExtract.py
@@ -1,16 +1,45 @@
 # This program extracts UF email addresses from a CSV file and prints them organized by class sections.
+import os
+import re
+
+COURSE_NAME = "EEE3308C"
+
+
 def extractEmailAddress(list):
     for item in list:
         if "@ufl.edu" in item:
             return item
     raise RuntimeError
 
-with open("grades.csv", "r") as studentInfo:
-    lines = [row.split(",") for row in studentInfo]
+
+def extractCVSInfo(namePattern):
+    csvFiles = [file for file in os.listdir('.') if re.match(namePattern,file)]
+    if len(csvFiles) > 1:
+        print("Multiple " + COURSE_NAME + " CSV files in the current directory: ", csvFiles)
+        print("Please make sure there is only one " + COURSE_NAME + " CSV file and try again.")
+        exit()
+    elif len(csvFiles) == 0:
+        print("No " + COURSE_NAME + " CSV file found in current directory. Please try again.")
+        exit()
+    try:
+        with open(csvFiles[0], "r") as studentInfo:
+            lines = [row.split(",") for row in studentInfo]
+    except EnvironmentError:
+        print("Failed to open the CSV file: ",csvFiles[0])
+        print("Please check the file and try again.")
+        exit()
+    return lines
+
+
+namePattern = r"\d+_\w+_\d+_\d+_Grades-" + COURSE_NAME + r"\.csv"
+lines = extractCVSInfo(namePattern)
 dict = {}
 for row in lines:
     for entry in row:
-        if "EEE3308C-" in entry:
+        if " Test\"" in entry: # search and remove test student
+            lines.remove(row)
+            break
+        if COURSE_NAME + "-" in entry:
             try:
                 email = extractEmailAddress(row)
             except:
@@ -20,6 +49,7 @@ for row in lines:
                 dict[entry] = [email]
             else:
                 dict[entry].append(email)
+print(COURSE_NAME + " Student Email Summary:",end="\n\n")
 print("All Emails", end=" ")
 print("(%d students)" % sum([len(value) for value in dict.values()]))
 for emails in dict.values():
